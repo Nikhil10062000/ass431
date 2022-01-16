901ca1a5de007d525d3ac350bb8817a75be2ee13
Nikhil10062000 <94104092+Nikhil10062000@users.noreply.github.com>
2022-01-16 12:25:30 +0530

    Add files via upload

---
+++ ass431.js
@@ -0,0 +1,58 @@
+//count from 0 variable
+var count = 0;
+//printing value from count
+document.getElementById("counter").innerText = count;
+// increment function like
+function like0() {
+	count = count + 1;
+	document.getElementById("counter").innerText = count;
+}
+// decrement function dislike
+function dislike0() {
+    count = count - 1;
+	document.getElementById("counter").innerText = count;
+}
+//count from 0 variable
+var count1 = 0;
+//printing value from count
+document.getElementById("counter1").innerText = count1;
+// increment function like
+function like1() {
+	count1 = count1 + 1;
+	document.getElementById("counter1").innerText = count1;
+}
+// decrement function dislike
+function dislike1() {
+    count1 = count1 - 1;
+	document.getElementById("counter1").innerText = count1;
+}
+//count from 0 variable
+var count2 = 0;
+//printing value from count
+document.getElementById("counter1").innerText = count2;
+// increment function like
+function like2() {
+	count2 = count2 + 1;
+	document.getElementById("counter2").innerText = count2;
+}
+// decrement function dislike
+function dislike2() {
+    count2 = count2 - 1;
+	document.getElementById("counter2").innerText = count2;
+}
+//count from 0 variable
+var count3 = 0;
+//printing value from count
+document.getElementById("counter3").innerText = count3;
+// increment function like
+function like3() {
+	count3 = count3 + 1;
+	document.getElementById("counter3").innerText = count3;
+}
+// decrement function dislike
+function dislike3() {
+    count3 = count3 - 1;
+	document.getElementById("counter3").innerText = count3;
+}
+
+
